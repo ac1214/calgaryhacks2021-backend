9f291825906aaa2ed3638008471b0f2553b6eaac
ac1214 <ac1214@users.noreply.github.com>
2021-02-13 16:24:15 -0700

    revert port

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def get_problems():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 1234))
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
